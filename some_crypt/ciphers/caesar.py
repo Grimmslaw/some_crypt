eec8351384dfaa89efb794934ec6db40a910c787
dwrickey <dwrickey@uab.edu>
2018-12-15 14:46:25 -0600

    finished caesar.py and caesar_tests.py

--- some_crypt/ciphers/caesar.py
+++ some_crypt/ciphers/caesar.py
@@ -1,29 +1,58 @@
+import re
 from util.functional import shift_with_wrap
 
 
-def __caesar_encrypt_str(plaintext_input, key: int) -> str:
+def __caesar_encrypt_str(plaintext: str, key: int) -> str:
+    # encrypt a `str` using a caesar shift, retaining formatting
     ciphertext = ""
 
-    for letter in plaintext_input:
+    for letter in plaintext:
         if 65 <= ord(letter) <= 90:
-            block = "upper"
+            ciphertext += shift_with_wrap(letter, key)
         elif 97 <= ord(letter) <= 122:
-            block = "lower"
+            ciphertext += shift_with_wrap(letter, key).lower()
         else:
-            block = "other"
-
-        if block == "lower":
-            res = shift_with_wrap(letter, key).lower()
-        elif block == "upper":
-            res = shift_with_wrap(letter, key)
-        else:
-            res = letter
-
-        ciphertext += res
+            ciphertext += letter
 
     return ciphertext
 
 
-def caesar_encrypt(plaintext_input, key: int):
-    # TODO: make this a dispatcher
-    return __caesar_encrypt_str(plaintext_input, key)
+def __caesar_encrypt_strip_frmt(plaintext: str, key: int) -> str:
+    # encrypt a `str` using a caesar shift, stripping formatting
+    ciphertext = ""
+    plaintext = (re.sub(r'[^a-zA-Z0-9]', "", plaintext)).upper()
+
+    for letter in plaintext:
+        if 65 <= ord(letter) <= 122:
+            ciphertext += shift_with_wrap(letter, key)
+        else:
+            ciphertext += letter
+
+    return ciphertext
+
+
+def caesar_encrypt(plaintext: str, key: int, strip_frmt=False) -> str:
+    """
+    Encrypt `:plaintext:` using a Caesar shift of size `:key:`.\n
+
+    By default this method retains formatting (case, spaces, punctuation).
+    For example, `caesar_encrypt('Some words.', 2)` => `'Uqog yqtfu.'`\n
+
+    If `:strip_frmt:` is `True`, instead, formatting is stripped, returning
+    an all-caps, no-spaces, no-punctuation `str`. For example,
+    `caesar_encrypt('Some words.', 2, strip_frmt=True)` => `'UQOGYQTFU'`.
+
+    :param str plaintext:
+        the text to be encrypted
+    :param int key:
+        the amount to shift `:plaintext:`
+    :param bool strip_frmt:
+        [opt; def=False] whether to strip formatting from `:plaintext:`
+
+    :return:
+        the encrypted `str`
+    """
+    if strip_frmt:
+        return __caesar_encrypt_strip_frmt(plaintext, key)
+    else:
+        return __caesar_encrypt_str(plaintext, key)
--- some_crypt/tests/caesar_tests.py
+++ some_crypt/tests/caesar_tests.py
@@ -8,26 +8,38 @@ class TestCaesarMethods(unittest.TestCase):
     def test_caesar_encrypt_min_key(self):
         res = caesar_encrypt("abc", 1)
         self.assertEqual(res, "bcd", f"Expected 'bcd'. Got '{res}'.")
+        res_strip = caesar_encrypt("abc", 1, strip_frmt=True)
+        self.assertEqual(res_strip, "BCD", f"Expected 'BCD'. Got '{res_strip}'.")
 
     def test_caesar_encrypt_zero_key(self):
         res = caesar_encrypt("abc", 0)
         self.assertEqual(res, "abc", f"Expected 'abc'. Got '{res}'.")
+        res_strip = caesar_encrypt("abc", 0, strip_frmt=True)
+        self.assertEqual(res_strip, "ABC", f"Expected 'BCD'. Got '{res_strip}'.")
 
     def test_caesar_encrypt_neg_key(self):
         res = caesar_encrypt("abc", -1)
         self.assertEqual(res, "zab", f"Expected 'abc'. Got '{res}'")
+        res_strip = caesar_encrypt("abc", -1, strip_frmt=True)
+        self.assertEqual(res_strip, "ZAB", f"Expected 'ZAB'. Got '{res_strip}'.")
 
     def test_caesar_encrypt_wrap_in_middle(self):
         res = caesar_encrypt("abc", 25)
         self.assertEqual(res, "zab", f"Expected 'zab'. Got '{res}'.")
+        res_strip = caesar_encrypt("abc", 25, strip_frmt=True)
+        self.assertEqual(res_strip, "ZAB", f"Expected 'ZAB'. Got '{res_strip}'.")
 
     def test_caesar_encrypt_wrap_from_start(self):
         res = caesar_encrypt("abc", 30)
         self.assertEqual(res, "efg", f"Expected 'efg'. Got '{res}'.")
+        res_strip = caesar_encrypt("abc", 30, strip_frmt=True)
+        self.assertEqual(res_strip, "EFG", f"Expected 'EFG'. Got '{res_strip}'.")
 
     def test_caesar_encrypt_with_space(self):
         res = caesar_encrypt("abc def", 1)
         self.assertEqual(res, "bcd efg", f"Expected 'bcd efg'. Got '{res}'.")
+        res_strip = caesar_encrypt("abc def", 1, strip_frmt=True)
+        self.assertEqual(res_strip, "BCDEFG", f"Expected 'BCDEFG'. Got '{res_strip}'.")
 
 
 if __name__ == '__main__':
